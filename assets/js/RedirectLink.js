afbf7944b2215b29739c80f875695c0421737230
Italo204 <italosantos.developer@gmail.com>
2025-03-07 18:40:55 -0300

    fix: Variaveis sensíveis protegidas

--- assets/js/RedirectLink.js
+++ assets/js/RedirectLink.js
@@ -1,25 +1,21 @@
 document.getElementById("Whatsapp").addEventListener("click", function(){
-    const phoneNumber = "5534988794217";
-    const url = `https://wa.me/${phoneNumber}`;
+    const url = config.PHONENUMBER;
     window.open(url, "_blank");
 });
 
 document.getElementById("Email").addEventListener("click", function(){
-    const email = "italosantos.developer@gmail.com";
     const subject = "Contato Profissional";
 
-    const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}`;
+    const mailtoLink = `mailto:${config.EMAIL}?subject=${encodeURIComponent(subject)}`;
     window.location.href = mailtoLink;
 });
 
 document.getElementById("Git").addEventListener("click", function() {
-    const userName = "Italo204"
-    const url = `https://www.github.com/${userName}`;
+    const url = config.GIT;
     window.open(url, "_blank");
 });
 
 document.getElementById("Linkedin").addEventListener("click", function() {
-    const user = "italo-developer"
-    const url = `https://www.linkedin.com/in/${user}/`
+    const url = config.LINKEDIN;
     window.open(url, "_blank");
 });
